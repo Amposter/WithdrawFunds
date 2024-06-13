eda6a31d5a0cc1bb97e1e840fae9903f3f6d3a7c
Aashiq Parker <amp940703@gmail.com>
2024-06-13 18:33:59 +0200

    introduces a property files for different environments and moves sns properties there

--- src/main/java/org/sanlam/config/AwsConfig.java
+++ src/main/java/org/sanlam/config/AwsConfig.java
@@ -1,15 +1,16 @@
-package org.sanlam.controllers.config;
+package org.sanlam.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 
 @Configuration
 public class AwsConfig {
 
-    private String awsRegion = "eu-west-1";
+    @Value("${aws.region}")
+    private String awsRegion;
 
     @Bean
     public SnsClient snsClient() {
--- src/main/java/org/sanlam/services/SnsService.java
+++ src/main/java/org/sanlam/services/SnsService.java
@@ -1,22 +1,26 @@
 package org.sanlam.services;
 
-import org.sanlam.events.WithdrawalEvent;
 import org.springframework.beans.factory.annotation.Autowired;
-import software.amazon.awssdk.regions.Region;
+import org.springframework.beans.factory.annotation.Value;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
 import software.amazon.awssdk.services.sns.model.PublishResponse;
 
 import java.math.BigDecimal;
 
+import org.sanlam.events.WithdrawalEvent;
+
+
 public class SnsService {
     @Autowired
     private SnsClient snsClient;
 
+    @Value("${aws.snsTopicArn}")
+    private String snsTopicArn;
+
     public void publishWithdrawalEvent(BigDecimal amount, Long accountId, String status) {
         WithdrawalEvent event = new WithdrawalEvent(amount, accountId, "SUCCESSFUL");
         String eventJson = event.toJson(); // Convert event to JSON
-        String snsTopicArn = "arn:aws:sns:YOUR_REGION:YOUR_ACCOUNT_ID:YOUR_TOPIC_NAME";
 
         PublishRequest publishRequest = PublishRequest.builder()
                 .message(eventJson)
