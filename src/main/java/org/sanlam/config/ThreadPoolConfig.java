61c3e2a5627d061974f0f821a88e2b355de9ed87
Aashiq Parker <amp940703@gmail.com>
2024-06-17 16:00:03 +0200

    adds custom threadpool to be used for database calls

---
+++ src/main/java/org/sanlam/config/ThreadPoolConfig.java
@@ -0,0 +1,21 @@
+package org.sanlam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+public class ThreadPoolConfig {
+
+    @Bean(name = "dbThreadPoolTaskExecutor")
+    public Executor dbThreadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(20);
+        executor.setThreadNamePrefix("productTaskExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
